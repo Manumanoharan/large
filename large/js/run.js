ec4187f822703242724b34f772b8f496599fe69a
Manu <90511900+Manumanoharan@users.noreply.github.com>
2022-07-16 19:30:40 +0530

    Add files via upload

---
+++ large/js/run.js
@@ -0,0 +1,21 @@
+function large(a,b)
+{
+   if (a<b)
+   {
+      large=('B is large number')
+   }
+else if (b<a)
+{
+   large=('a is large number')
+}
+   return large;
+   
+
+}
+var a=15;
+var b=10;
+
+
+
+var l=large(a,b);
+console.log (l);
